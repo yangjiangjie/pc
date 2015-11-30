9036368bb4a6896bdde9de2716a33be3f83b2176
jiege <1192322386@qq.com>
2015-12-01 06:20:51 +0800

    change main.c

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main(int argc,char *argv[])
 {
-	printf("%s,%s,%d\n",__FILE__,__FUNCTION__,__LINE__);//
+	FILE * fp=NULL;
+	printf("%s,%s,%d\n",__FILE__,__FUNCTION__,__LINE__);
+	fp=fopen("config.ini");
+	if(fp==NULL)return 0;
+	fclose(fp);
 	return 0;
 }
